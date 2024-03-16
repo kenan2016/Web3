5cac50016b3a71f5b9544218be954613fa454412
xiawenyu <xwy524871763qq@gmail.com>
2024-03-16 14:13:30 +0800

    const
    const value

---
+++ Solidity-demo/InsertionSort.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+contract InsertionSort {
+    // / if else
+    function ifElseFunc(uint a, uint b) public pure returns(bool) {
+        if(a>b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
