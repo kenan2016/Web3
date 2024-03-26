b24c7df95a4058023bafda22005140a7b4ecd91f
xiawenyu <xwy524871763qq@gmail.com>
2024-03-26 22:58:24 +0800

    sort
    sort

---
+++ Solidity-demo/Base.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+abstract contract Base{
+    // string public name = "Base";
+    function getAlias() public pure virtual returns(string memory);
+}
+
+contract BaseImpl is Base{
+    function getAlias() public pure override returns(string memory){
+        return "BaseImpl";
+    }
+}
